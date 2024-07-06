5249b6c6eee0584c4bb85f6f2601b2ede430aae1
Avanish Kumar Singh <avanishkumarsingh@Avanishs-MacBook-Air.local>
2024-07-06 13:43:06 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["flowbite.s3.amazonaws.com", 'images.unsplash.com'],
+    },
+  };
+export default nextConfig;
